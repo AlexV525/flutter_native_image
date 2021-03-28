f3f1f195c63fccfd28cc1fd44156efe523ef45a9
Alex <github@alexv525.com>
2021-03-28 22:19:10 +0800

    Support nnbd

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,26 +4,21 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
 
   @override
-  initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Plugin example app'),
         ),
-        body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
+        body: Center(
+          child: Text('Running on: $_platformVersion\n'),
         ),
       ),
     );
--- lib/flutter_native_image.dart
+++ lib/flutter_native_image.dart
@@ -4,8 +4,7 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class FlutterNativeImage {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_native_image');
+  static const MethodChannel _channel = MethodChannel('flutter_native_image');
 
   /// Compress an image
   ///
@@ -14,56 +13,41 @@ class FlutterNativeImage {
   /// [quality] controls how strong the compression should be. (0-100)
   /// Use [targetWidth] and [targetHeight] to resize the image for a specific
   /// target size.
-  static Future<File> compressImage(String fileName,
-      {int percentage = 70,
-      int quality = 70,
-      int targetWidth = 0,
-      int targetHeight = 0}) async {
-    var file = await _channel.invokeMethod("compressImage", {
+  static Future<File?> compressImage(
+    String fileName, {
+    int percentage = 70,
+    int quality = 70,
+    int targetWidth = 0,
+    int targetHeight = 0,
+  }) async {
+    final String? path = await _channel.invokeMethod('compressImage', {
       'file': fileName,
       'quality': quality,
       'percentage': percentage,
       'targetWidth': targetWidth,
       'targetHeight': targetHeight
     });
-
-    return new File(file);
+    if (path == null) {
+      return null;
+    }
+    return File(path);
   }
 
   /// Gets the properties of an image
   ///
   /// Gets the properties of an image given the [fileName].
   static Future<ImageProperties> getImageProperties(String fileName) async {
-    ImageOrientation decodeOrientation(int orientation) {
-      // For details, see: https://developer.android.com/reference/android/media/ExifInterface
-      switch (orientation) {
-        case 1:
-          return ImageOrientation.normal;
-        case 2:
-          return ImageOrientation.flipHorizontal;
-        case 3:
-          return ImageOrientation.rotate180;
-        case 4:
-          return ImageOrientation.flipVertical;
-        case 5:
-          return ImageOrientation.transpose;
-        case 6:
-          return ImageOrientation.rotate90;
-        case 7:
-          return ImageOrientation.transverse;
-        case 8:
-          return ImageOrientation.rotate270;
-        default:
-          return ImageOrientation.undefined;
-      }
-    }
-
-    var properties = Map.from(
-        await _channel.invokeMethod("getImageProperties", {'file': fileName}));
-    return new ImageProperties(
-        width: properties["width"],
-        height: properties["height"],
-        orientation: decodeOrientation(properties["orientation"]));
+    final Map<dynamic, dynamic> properties = Map<dynamic, dynamic>.from(
+      await _channel.invokeMethod(
+        'getImageProperties',
+        <String, dynamic>{'file': fileName},
+      ),
+    );
+    return ImageProperties(
+      width: properties['width'],
+      height: properties['height'],
+      orientation: _decodeOrientation(properties['orientation']),
+    );
   }
 
   /// Crops an image
@@ -71,21 +55,56 @@ class FlutterNativeImage {
   /// Crops the given [fileName].
   /// [originX] and [originY] control from where the image should be cropped.
   /// [width] and [height] control how the image is being cropped.
-  static Future<File> cropImage(
-      String fileName, int originX, int originY, int width, int height) async {
-    var file = await _channel.invokeMethod("cropImage", {
-      'file': fileName,
-      'originX': originX,
-      'originY': originY,
-      'width': width,
-      'height': height
-    });
+  static Future<File?> cropImage(
+    String fileName,
+    int originX,
+    int originY,
+    int width,
+    int height,
+  ) async {
+    final String? path = await _channel.invokeMethod(
+      'cropImage',
+      <String, dynamic>{
+        'file': fileName,
+        'originX': originX,
+        'originY': originY,
+        'width': width,
+        'height': height
+      },
+    );
 
-    return new File(file);
+    if (path == null) {
+      return null;
+    }
+    return File(path);
+  }
+
+  static ImageOrientation _decodeOrientation(int orientation) {
+    // For details, see: https://developer.android.com/reference/android/media/ExifInterface
+    switch (orientation) {
+      case 1:
+        return ImageOrientation.normal;
+      case 2:
+        return ImageOrientation.flipHorizontal;
+      case 3:
+        return ImageOrientation.rotate180;
+      case 4:
+        return ImageOrientation.flipVertical;
+      case 5:
+        return ImageOrientation.transpose;
+      case 6:
+        return ImageOrientation.rotate90;
+      case 7:
+        return ImageOrientation.transverse;
+      case 8:
+        return ImageOrientation.rotate270;
+      default:
+        return ImageOrientation.undefined;
+    }
   }
 }
 
-/// Imageorientation enum used for [getImageProperties].
+/// Image orientation enum used for [getImageProperties].
 enum ImageOrientation {
   normal,
   rotate90,
@@ -100,12 +119,13 @@ enum ImageOrientation {
 
 /// Return value of [getImageProperties].
 class ImageProperties {
-  int width;
-  int height;
-  ImageOrientation orientation;
+  const ImageProperties({
+    this.width = 0,
+    this.height = 0,
+    this.orientation = ImageOrientation.undefined,
+  });
 
-  ImageProperties(
-      {this.width = 0,
-      this.height = 0,
-      this.orientation = ImageOrientation.undefined});
+  final int width;
+  final int height;
+  final ImageOrientation orientation;
 }
